2feddfea76bf52ef467c79b3f9ddca59f9c646a1
syeyoung <cyoung06@naver.com>
2024-08-05 21:28:24 +0900

    - announcement board

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <div class="footer">
-    <span>Made with ♥ from 13기 송시영 | 세마고등학교 프로그래밍 동아리 세미콜론에서 운영중</span>
+    <span>Made with ♥ from 13기 송시영 | 세마고등학교 프로그래밍 동아리 세미콜론에서 운영중 | Report issues at <a href="https://github.com/semahs-semicolon/inventory-front/issues">Github Issues</a></span>
 </div>
 
 <style>
--- src/routes/dashboard/notice/+page.svelte
+++ src/routes/dashboard/notice/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import { preloadData } from "$app/navigation";
+	import { goto } from "$app/navigation";
 	import PrimaryButton from "../../../components/button/PrimaryButton.svelte";
-import Footer from "../../../components/Footer.svelte";
+    import Footer from "../../../components/Footer.svelte";
+	import { AUTHORITIES } from "../../../stores";
 
+    export let data;
 </script>
 
 
@@ -9,14 +13,50 @@ import Footer from "../../../components/Footer.svelte";
     <div class="wrapper">
         <span class="title">공지
 
-            <PrimaryButton>글 쓰기</PrimaryButton>
+            {#if $AUTHORITIES.includes("CREATE_NOTICE")}
+            <PrimaryButton on:click={() => {goto("/dashboard/notice/new")}}>글 쓰기</PrimaryButton>
+            {/if}
         </span>
         <hr/>
+        {#each data.notices as notice}
+            <div class="notice" on:click={() => goto(`/dashboard/notice/${notice.id}`)} on:mouseover={() => preloadData(`/dashboard/notice/${notice.id}`)}>
+                <span class="header">
+                    <span class="title">{notice.title}</span>
+                    <span class="author">{notice.authorName}</span>
+                    <span class="date">{new Date(notice.date).toLocaleDateString('ko')}</span>
+                </span>
+
+                <span class="content">{notice.content}</span>
+            </div>
+        {/each}
     </div>
 <Footer/>
 </div>
 
 <style>
+    .header {
+        display: flex;
+        flex-direction: row;
+        align-items: end;
+        padding-bottom: 0.5em;
+        gap: 0.5em;
+    }
+    .notice {
+        display: flex;
+        flex-direction: column;;
+        padding: 1em;
+        border-color: #DEDEDE;
+        border-style: solid;
+        border-radius: 0.5em;
+        border-width: 1px;
+        cursor: pointer;
+    }
+    .notice:hover {
+        background-color: #EEEEEE;
+    }
+    .notice:active {
+        background-color: #DEDEDE;
+    }
     .wrapper {
         display: flex;
         flex-direction: column;
@@ -28,6 +68,7 @@ import Footer from "../../../components/Footer.svelte";
         margin-right: auto;
         width: 100%;
         background-color: #FFFFFF;
+        gap: 1em;
     }
     .tall {
         display: flex;
@@ -40,6 +81,15 @@ import Footer from "../../../components/Footer.svelte";
         font-size: 1.5em;
         gap: 0.5em;
         justify-content: space-between;
+    }
+    .content {
+        color: gray;
+    }
+    .date {
+        color: gray;
+    }
+    .author {
+        color: gray;
     }
     hr {
         margin-left: 0;
