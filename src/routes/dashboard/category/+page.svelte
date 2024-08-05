c550edc5b5389a62289f4557e09cc74bc81dc435
syeyoung <cyoung06@naver.com>
2024-08-05 19:52:20 +0900

    ai recategorization has been removed

--- src/routes/dashboard/category/+page.svelte
+++ src/routes/dashboard/category/+page.svelte
@@ -11,25 +11,26 @@
     console.log(data);
     
 
-    const requestAI = async () => {
-        const yesorno = confirm("정말로 AI 분류를 시작하시겠습니까?\n\n물품 추가시 AI분류는 기본적으로 자동으로 이루어지며, 이 작업을 시작할 경우, 모든 물품에 대해 AI분류를 다시 수행합니다.")
-        if (!yesorno) return;
+    // const requestAI = async () => {
+    //     const yesorno = confirm("정말로 AI 분류를 시작하시겠습니까?\n\n물품 추가시 AI분류는 기본적으로 자동으로 이루어지며, 이 작업을 시작할 경우, 모든 물품에 대해 AI분류를 다시 수행합니다.")
+    //     if (!yesorno) return;
 
-        await authfetch(`${API_URL()}/categories/ai`, {
-            method: "POST"
-        });
-        invalidateAll();
-    }
+    //     // await authfetch(`${API_URL()}/categories/ai`, {
+    //     //     method: "POST"
+    //     // });
+    //     // invalidateAll();
+    //     alert("AI 재분류는 너무 비싸져서 ")
+    // }
 
-    const stopAI = async () => {
-        const yesorno = confirm("정말로 AI 재분류를 멈추시겠습니까?")
-        if (!yesorno) return;
+    // const stopAI = async () => {
+    //     const yesorno = confirm("정말로 AI 재분류를 멈추시겠습니까?")
+    //     if (!yesorno) return;
 
-        await authfetch(`${API_URL()}/categories/ai`, {
-            method: "DELETE"
-        });
-        invalidateAll();
-    }
+    //     await authfetch(`${API_URL()}/categories/ai`, {
+    //         method: "DELETE"
+    //     });
+    //     invalidateAll();
+    // }
 </script>
 
 <div class="wrapper">
@@ -41,7 +42,7 @@
         "parent_category_id": null,
         "products": data.products
     }} childCategories={data.categories}>
-        {#if data.ai.running}
+        <!-- {#if data.ai.running}
         <span>
             재분류중... {data.ai.completed}/{data.ai.total}
         </span>
@@ -56,7 +57,7 @@
         {:else}
             <PrimaryButton
                 on:click={requestAI}>AI 재분류 하기 (~1시간)</PrimaryButton>
-        {/if}
+        {/if} -->
         <PrimaryButton on:click={() => goto("/dashboard/categoryConfirm")}>최근 추가된 물품</PrimaryButton>
     </CategorySelector>
 </div>
