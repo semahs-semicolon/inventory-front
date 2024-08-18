a99546058909fd3f015b5702c89afe8e473e6f5c
thezoot3 <thezoot3@gmail.com>
2024-08-19 08:16:47 +0900

    Add Alert System

---
+++ src/components/Alert/Alerts.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { AlertList } from '../../stores/AlertPopup.js';
+	import AlertElement from './AlertElement.svelte';
+
+	export const maxAlertNumber = 3;
+
+	let alerts = [];
+
+	AlertList.subscribe((value) => {
+		let temp = [];
+		value.reverse().forEach((alert) => {
+			if (temp.length < maxAlertNumber) {
+				temp.push(alert);
+			}
+		});
+		alerts = temp;
+	});
+</script>
+
+<div class="absolute bottom-0 left-0 flex flex-col gap-5 p-6">
+	{#each alerts as alert}
+		<AlertElement alertID={alert.id} {alert} />
+	{/each}
+</div>
