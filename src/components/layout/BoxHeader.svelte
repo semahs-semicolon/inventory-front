cdbb52e728491956f31e3319d823b745eaeac138
thezoot3 <thezoot3@gmail.com>
2024-08-19 08:22:48 +0900

    Add useful layout component

---
+++ src/components/layout/BoxHeader.svelte
@@ -0,0 +1,4 @@
+<div class="flex max-h-16 w-full items-center justify-between bg-gray-200 py-3 pl-5 pr-2">
+	<span class="text-lg font-semibold"><slot name="title" /></span>
+	<slot name="other" />
+</div>
---
+++ src/components/layout/CenterAlign.svelte
@@ -0,0 +1,3 @@
+<div class="relative flex h-full w-full items-center justify-center">
+	<slot />
+</div>
