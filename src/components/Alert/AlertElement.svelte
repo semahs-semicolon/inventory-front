479568ce62e3e9a76c0c27ad5dfd9c9bcfd4201d
thezoot3 <thezoot3@gmail.com>
2024-08-20 17:20:57 +0900

    Fix Alert Feature

--- src/components/Alert/AlertElement.svelte
+++ src/components/Alert/AlertElement.svelte
@@ -14,33 +14,34 @@
 	};
 
 	const colorPalette = {
-		info: 'slate-400',
-		warning: 'yellow-400',
-		error: 'red-500'
+		info: 'bg-slate-400',
+		warning: 'bg-yellow-400',
+		error: 'bg-red-500'
+	};
+	const colorSpanPalette = {
+		info: 'white',
+		warning: 'white',
+		error: 'white'
 	};
 </script>
 
 {#if alert.message.length > 0}
-	<div
-		class={`alertIntroAnim relative flex gap-4 rounded-xl bg-${
-			colorPalette[alert.level]
-		} px-6 py-4 shadow-xl`}
-	>
+	<div class={`alertIntroAnim relative flex gap-4 rounded-xl ${colorPalette[alert.level]} px-6 py-4 shadow-xl`}>
 		<span
-			class={`material-symbols-outlined absolute -left-3 -top-3 flex h-[2rem] w-[2rem] animate-bounce items-center justify-center rounded-2xl bg-${
+			class={`material-symbols-outlined absolute -left-3 -top-3 flex h-[1.5rem] w-[1.5rem] animate-bounce items-center justify-center rounded-2xl ${
 				colorPalette[alert.level]
 			} text-lg text-white drop-shadow-xl`}>priority_high</span
 		>
 		{#if alert.removable}
 			<button
-				class="material-symbols-outlined absolute -right-2 -top-2 flex h-[2rem] w-[2rem] items-center justify-center rounded-2xl bg-gray-200 text-lg font-semibold text-gray-700 shadow-xl"
+				class="material-symbols-outlined absolute -right-2 -top-2 flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-2xl bg-gray-200 text-base font-semibold text-gray-700 shadow-xl"
 				on:click={closeAlert}>close</button
 			>
 		{/if}
 		<div class="flex flex-col text-white">
-			<span class="text-lg font-medium">{alert.message}</span>
+			<span class="text-base font-medium">{alert.message}</span>
 			{#if alert.description}
-				<span class="text-base font-normal text-gray-100">{alert.description}</span>
+				<span class="text-sm font-normal text-gray-100">{alert.description}</span>
 			{/if}
 		</div>
 	</div>
