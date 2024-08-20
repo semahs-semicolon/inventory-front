7b196c81a1bdc9d98640f7ef5aa383b2707f9cd6
thezoot3 <thezoot3@gmail.com>
2024-08-20 17:31:06 +0900

    Server side rendering or prerendering

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,9 @@
 import { get } from 'svelte/store';
 import { ACCESS_TOKEN } from '../stores/AccessToken.js';
-import { redirect } from '@sveltejs/kit';
 import { goto } from '$app/navigation';
 
-export const ssr = false;
 export const csr = true;
-export const prerender = false;
+export const prerender = true;
 
 export async function load() {
 	if (get(ACCESS_TOKEN) !== null) {
